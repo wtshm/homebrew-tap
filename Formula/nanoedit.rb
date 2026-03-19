9ac160d2a0a6569fc698a8fcf71e4b7c9e7b5b14
Kenta Watashima <kenta@achr.me>
2026-03-19 12:46:26 +0900

    Add nanoedit formula

---
+++ Formula/nanoedit.rb
@@ -0,0 +1,19 @@
+class Nanoedit < Formula
+  desc "Minimal floating text editor designed for use as an external editor"
+  homepage "https://github.com/wtshm/nanoedit"
+  url "https://github.com/wtshm/nanoedit/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "f57e6893f7d38f8898def687b5ccd4b288f4fcfdbeaee6820090adc95d6b44b6"
+  license "MIT"
+
+  depends_on :macos
+  depends_on xcode: ["14.0", :build]
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/nanoedit"
+  end
+
+  test do
+    assert_match "Usage: nanoedit", shell_output("#{bin}/nanoedit 2>&1", 1)
+  end
+end
