c71f6c2b6e498c613073922682ec5bf87a37fa25
Kenta Watashima <kenta@achr.me>
2026-03-19 15:34:51 +0900

    Update nanoedit sha256 checksum

--- Formula/nanoedit.rb
+++ Formula/nanoedit.rb
@@ -2,7 +2,7 @@ class Nanoedit < Formula
   desc "Minimal floating text editor designed for use as an external editor"
   homepage "https://github.com/wtshm/nanoedit"
   url "https://github.com/wtshm/nanoedit/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "f57e6893f7d38f8898def687b5ccd4b288f4fcfdbeaee6820090adc95d6b44b6"
+  sha256 "f8b509c9c22dd9f5c64df8bcaeb7b8f2e9b54ab008d33bda00d599452a11dfbd"
   license "MIT"
 
   depends_on :macos
